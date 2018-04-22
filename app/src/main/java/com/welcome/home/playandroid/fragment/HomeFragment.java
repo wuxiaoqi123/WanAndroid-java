ec78b38512b25b1f86d4c703968cf63a0887cc6f
wuxiaoqi <1321972760@qq.com>
2018-04-22 22:24:45 +0800

    修改布局文件

--- app/src/main/java/com/welcome/home/playandroid/fragment/HomeFragment.java
+++ app/src/main/java/com/welcome/home/playandroid/fragment/HomeFragment.java
@@ -1,9 +1,13 @@
 package com.welcome.home.playandroid.fragment;
 
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 
+import com.scwang.smartrefresh.layout.SmartRefreshLayout;
 import com.welcome.home.playandroid.R;
 import com.welcome.home.playandroid.base.BaseFragment;
+
+import butterknife.BindView;
 
 /**
  * Created by wuxiaoqi on 2018/4/20.
@@ -15,6 +19,12 @@ public class HomeFragment extends BaseFragment {
     public static HomeFragment getInstance() {
         return new HomeFragment();
     }
+
+    @BindView(R.id.refresh_content_smart_layout)
+    SmartRefreshLayout smartRefreshLayout;
+
+    @BindView(R.id.refresh_content_recyclerview)
+    RecyclerView recyclerView;
 
     @Override
     protected int getLayoutId() {
